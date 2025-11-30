64dfcff5e77cdd894a0deb990bd3b2dceef3475e
Dion <dionang11@gmail.com>
2025-11-30 23:43:13 +0800

    toolbar color

--- app/components/shared/GrainOverlay.tsx
+++ app/components/shared/GrainOverlay.tsx
@@ -1,9 +1,9 @@
-import { GRAIN_OVERLAY_SVG } from '@/lib/constants/grainOverlay';
+import { GRAIN_OVERLAY_SVG } from "@/lib/constants/grainOverlay";
 
 export function GrainOverlay() {
   return (
     <div
-      className="absolute inset-0 opacity-40 pointer-events-none rounded-full bg-white/10"
+      className="absolute inset-0 opacity-40 pointer-events-none rounded-full bg-gray-500/30"
       style={{
         backgroundImage: GRAIN_OVERLAY_SVG,
         backgroundRepeat: "repeat",
